9fd25a8e5988029cf912c8aa8f55ecf4c7933147
ethansagin <ethansagin@gmail.com>
2019-07-15 12:01:34 -0700

    devise / oauth set up complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :registrations => "registrations", :omniauth_callbacks => "callbacks" }
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
