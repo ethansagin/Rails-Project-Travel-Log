613acc9dc1cd9a8bc0be03ad99425d16bcf0a7df
ethansagin <ethansagin@gmail.com>
2019-07-19 14:03:56 -0700

    add search bar logic to destinations controller

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -2,7 +2,13 @@ class DestinationsController < ApplicationController
     before_action :authenticate_user!, :set_user
 
     def index
-        @destinations = @user.destinations
+        binding.pry
+        if params.has_key?(:destination) && !params[:destination][:country_id].empty?
+            search = params[:destination][:country_id]
+            @destinations = @user.destinations.where(country_id: search)
+        else
+            @destinations = @user.destinations
+        end
     end
 
     def new
